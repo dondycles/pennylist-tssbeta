cc7e4186e546c2d1cd96c658814d19af1250cd78
dondycles <johnrod.dondoyano@gmail.com>
2025-06-13 21:51:52 +0800

    middleware

--- src/lib/middleware/auth-guard.ts
+++ src/lib/middleware/auth-guard.ts
@@ -2,22 +2,18 @@ import { createMiddleware } from "@tanstack/react-start";
 import { setResponseStatus } from "@tanstack/react-start/server";
 import { getSupabaseServerClient } from "../server/supabase";
 
-// https://tanstack.com/start/latest/docs/framework/react/middleware
-// This is a sample middleware that you can use in your server functions.
+export const authMiddleware = createMiddleware({ type: "function" }).server(
+  async ({ next }) => {
+    const supabase = getSupabaseServerClient();
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
 
-/**
- * Middleware to force authentication on a server function, and add the user to the context.
- */
-export const authMiddleware = createMiddleware().server(async ({ next }) => {
-  const supabase = getSupabaseServerClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+    if (!user) {
+      setResponseStatus(401);
+      throw new Error("Unauthorized");
+    }
 
-  if (!user) {
-    setResponseStatus(401);
-    throw new Error("Unauthorized");
+    return next({ context: { user } });
   }
-
-  return next({ context: { user } });
-});
+);
