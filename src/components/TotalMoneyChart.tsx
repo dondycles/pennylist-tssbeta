3cb8d711935220e534f5b0748e04709e823cde3e
dondycles <johnrod.dondoyano@gmail.com>
2025-06-13 18:52:19 +0800

    newer design for analytics

--- src/components/TotalMoneyChart.tsx
+++ src/components/TotalMoneyChart.tsx
@@ -53,7 +53,7 @@ export function TotalMoneyChart({
   dateJoined: Date;
 }) {
   return (
-    <div className="space-y-4">
+    <div className="896:space-y-4 -my-4 896:my-0">
       <FlowChartCard
         data={data}
         dateJoined={dateJoined}
